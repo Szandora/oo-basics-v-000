3a8a624d37090c9685d6230e8f5866d6d3f0120a
Sheila Wittman <szandora789@gmail.com>
2018-06-10 16:58:28 +0000

    automatically backed up by learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,12 +1,9 @@
 class Book
   attr_accessor :author, :page_count, :genre
+  attr_accessor :title
 
   def initialize(title)
     @title = title
-  end
-
-  def title
-    @title
   end
 
   def turn_page
